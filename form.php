0ea3c038bb0c74d3d0b693584f1ee727f6d1cf3a
Gabriel Ribeiro <74364491+ChickenPox-Mucura@users.noreply.github.com>
2021-12-10 20:07:10 -0400

    first commit

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="action.php" method="post">
+<p>Nome: <input type="text" name="name" /></p>
+<p>Idade: <input type="text" name="idade" /></p>
+<p> <input type="submit" /></p>
+</form>
---
+++ site.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html lang=en>
+	<head>
+		<style>
+			body {
+				background-color: gray;
+				font-family: "MxPlus ToshibaTxL2 8x16";
+						}
+			header {
+				border: solid;
+				border-radius: 25px;
+				margin: 20px;
+				background-image: linear-gradient(darkgray,lightgray);
+						}
+			.sidenav {
+				color: lightgreen;
+				padding: 5px;
+						}
+			a { color: lightgreen; }
+		</style>
+		<meta charset=utf-8>
+		<title>lainbot.c's blog</title>
+	</head>
+	<body>
+		<div class="sidenav">
+			<a href="#">More</a>
+			<a href="#">Shoots & Ladders</a>
+			<a href="#">Justin</a>
+			<a href="#">Helmet in the Bush</a>
+			<a href="form.php">The Form</a>
+		</div>
+		<header>
+			<h1><center>lainbot.c's blog</center></h1>
+		</header>
+		<article>
+			<h2>Day 1</h2>
+			<p style="margin: 20px;">Tweaking with html and CSS has been a fun experience. A few more
+			   days and i'll have a nice looking blog to post about my not-so-interesting life.
+			   This is a project i wanted to do since i became conscious of my web pesantry,
+			   and it is nearing completion, provided i get a domain soon!</p>
+			<h2>Day 2</h2>
+			<p style="margin: 20px;">Still building the site. Learned how to make a simple fixed sidebar to place
+			   some links redirecting nowhere.</p>
+			<figure> 
+				<img src="120.jpg"
+				alt="My CGI home"
+				height="240"
+				width="350">
+				<figcaption style="color:#111;"><i>My CGI home</i></figcaption>
+			</figure>
+	</article>
+	</body>
+</html>
